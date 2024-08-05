1262e97b30dfac7f699360c5897ff32c8bdbba3a
Gabriel Dutra <bidielandrade@gmail.com>
2024-08-05 18:12:03 -0300

    código de merda

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("olá!!");
+let idade = prompt(`Digite qual é sua idade`);
+
+
+console.log(`Minha idade é ${idade} `)
+// alert(`Minha idade é ${idade} `)
+
+const result = document.getElementsByClassName("result");
+result[0].innerText = `Minha idade é ${idade} `;

316dc00a6ffb0c91ec6e96c335ad66b14cb0f7fd
Gabriel Dutra <bidielandrade@gmail.com>
2024-08-05 19:04:31 -0300

    exercicio 2 que eu espero estar correto

---
+++ Exercicio_2/script.js
@@ -0,0 +1,8 @@
+const valorMedia = 6;
+
+
+const notas = document.getElementsByClassName("nota");
+const media = document.getElementsByClassName("media");
+
+
+media[0].innerText = `A media é ${valorMedia}`
